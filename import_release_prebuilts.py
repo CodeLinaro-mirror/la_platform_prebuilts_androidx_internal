bd64dfbfa4acb9fbac6a9da9a99e4e2ed3ee5d5a
Julia McClellan <juliamcclellan@google.com>
2023-06-26 10:37:32 -0400

    Add more keywords_to_ignore to the prebuilts import script
    To prevent entries removed in aosp/2637429 from being added back.
    
    Test: `python3 test_import_release_prebuilts.py`
    Bug: 288582619
    
    Change-Id: I2336789bdc1f61cbbb640fcce06358b08df694cc

--- import_release_prebuilts.py
+++ import_release_prebuilts.py
@@ -312,7 +312,11 @@ def should_update_docs(new_maven_coordinates):
 		"iosarm64",
 		"iossimulatorarm64",
 		"iosx64",
-		"linuxx64"
+		"linuxx64",
+		"tools-apigenerator",
+		"tools-apipackager",
+		"tools-core",
+		"-proto"
 	]
 	coordinates_after_androidx = new_maven_coordinates.replace("androidx.", "")
 	for keyword in keywords_to_ignore:
